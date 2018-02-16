8ad92e3e73c9c4c2b1970e3a95823956b7040883
Shivendra Srivastava <35077718+shivendra1209@users.noreply.github.com>
2018-02-17 05:05:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,9 +19,9 @@ var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight() {
     marginLeft=marginLeft+10;
-    img.style.marginleft=marginLeft+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function() {
-    var interval = setInterval(moveRight,100);
+    var Interval = setInterval(moveRight,100);
     
 };
