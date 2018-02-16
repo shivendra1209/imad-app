ed96bf08bc2b984c31ba480d03c3b489d08c8cfa
Shivendra Srivastava <35077718+shivendra1209@users.noreply.github.com>
2018-02-17 04:46:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML="New VAlue";
+
+
+//Moving the picture on clicking
+
+var img = document.getElementById('madi');
+img.onclick=function() {
+    img.style.marginLeft='q00px';
+    
+};
