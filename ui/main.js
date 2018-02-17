f5544a4f2ac27df666b1749e250cfa4c1eea05ab
Shivendra Srivastava <35077718+shivendra1209@users.noreply.github.com>
2018-02-18 00:27:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,20 @@
+/*
 console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML="New VAlue";
 
 
-//Moving the picture on clicking
-/*
+//Moving the picture on clicking-----
+
 var img = document.getElementById('madi');
 img.onclick=function() {
     img.style.marginLeft='100px';
     
 };
-*/
 
-//Applying the gradual animation to the image
+
+//Applying the gradual animation to the image--------
 
 var img = document.getElementById('madi');
 var marginLeft=0;
@@ -25,3 +26,18 @@ img.onclick=function() {
     var Interval = setInterval(moveRight,100);
     
 };
+
+*/
+
+
+
+//counter code
+var button = document.getElementById('counter');
+var counter=0;
+
+button.onclick = function() {
+    counter=counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML=(counter.toString());
+};
+    
