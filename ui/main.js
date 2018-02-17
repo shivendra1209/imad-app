8c0e8cf6643585787037d48a3c668661a8caa0ff
Shivendra Srivastava <35077718+shivendra1209@users.noreply.github.com>
2018-02-18 01:22:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,8 +36,25 @@ var button = document.getElementById('counter');
 var counter=0;
 
 button.onclick = function() {
-    counter=counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML=(counter.toString());
+    
+    // creating a request
+    
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in the variable
+    
+    request.onreadystatechange=function(){
+        if(request.readystate===XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status==200) {
+                var counter = request.responseText;
+                 var span = document.getElementById('count');
+                span.innerHTML=(counter.toString());             
+            }
+        }
+    }
+
+request.open('GET','http://shivendra12091999.imad.hasura-app.io/counter',true);
+request.send(null);
 };
     
