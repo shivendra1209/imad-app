75bf29cc3354dbc2327348db6048a0825a1a5511
Shivendra Srivastava <35077718+shivendra1209@users.noreply.github.com>
2018-02-18 22:09:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,4 +57,31 @@ button.onclick = function() {
 request.open('GET','http://shivendra12091999.imad.hasura-app.io/counter',true);
 request.send(null);
 };
-    
+  
+  
+  //submit-name code
+  
+  var submit=document.getElementById('submit_btn');
+  submit.onclick = function() {
+      var request = new XMLHttpRequest(); //request is created
+      
+      request.onreadystatechange = function() { // function will execute on ready state change
+      if (request.readyState === XMLHttpRequest.DONE) {
+          if(request.Status === 200) {
+              var names= request.responseText;
+              names=JSON.parse(names);
+              var list='';
+              for(var i=0;i<names.length;i++) {
+                  list +='<li>'+names[i]+'</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML=list;
+          }
+      }
+          
+      };
+            var nameInput = document.getElementById('name');
+            var name = nameInput.value;
+            request.open('GET','http://shivendra12091999.imad.hasura-app.io/submit-name?name='+name,true);
+            request.send(null);
+  };
