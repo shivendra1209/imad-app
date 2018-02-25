2981f4eb8456f6cf2582118fdcbc2449057a632c
Shivendra Srivastava <35077718+shivendra1209@users.noreply.github.com>
2018-02-26 03:25:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var Pool = require('pg').Pool;
 
 var config={
     user:'shivendra12091999',
-    database:'shivemdra12091999',
+    database:'shivendra12091999',
     host:'db.imad.hasura-app.io',
     port:'5432',
     password:process.env.DB_PASSWORD
